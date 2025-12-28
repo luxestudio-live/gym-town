bd32c73154a2a24d18faf94b4ea74901ad253168
the-rahulk <the-rahulk@users.noreply.github.com>
2025-12-28 20:03:04 +0000

    deploy: 186063a9612b9a8e3803fa781b7db4ae45d33fbc

---
+++ _next/static/wx-Rd7fczvp8l5H-NGuSS/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/gym-town//_next/:path+",
+        "destination": "/gym-town/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
